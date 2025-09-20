18024fb278788aeb03d624f70bb8bca2669fbc2d
Jacob Wujciak-Jens <jacob@wujciak.de>
2025-09-20 17:24:23 +0200

    fix(fish): minor fixes

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,6 +1,7 @@
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/go/bin
 fish_add_path $HOME/.pixi/bin
+fish_add_path $HOME/.config/emacs/bin
 
 abbr --add python python3
 abbr --add va "source .venv/bin/activate.fish"
@@ -8,10 +9,17 @@ abbr --add vd deactivate
 abbr --add ia install_archery
 abbr --add dr "docker run --rm -it"
 abbr --add ope "op run --env-file .env --"
+abbr --add cz chezmoi
+if command -q helix
+  abbr --add hx helix
+end
 
 mise activate fish | source
 fzf --fish | source
 starship init fish | source
 zoxide init --cmd cd fish | source
 
-source /home/jwj/.config/op/plugins.sh
+set op_plugins $HOME/.config/op/plugins.sh
+if test -e $op_plugins
+    source $op_plugins
+end
